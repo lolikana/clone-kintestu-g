b7623692bed48ea8d5ffa6520af55fbea26ae220
Lolikana <thomas.guiart@gmail.com>
2023-10-03 08:15:02 +0900

    add build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+	build: {
+		inlineStylesheets: 'auto'
+	},
 	vite: {
 		resolve: {
 			alias: {
