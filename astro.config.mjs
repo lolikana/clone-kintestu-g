5bb88ca4b94d2365a9e8ce100928d4ba8e269201
Lolikana <thomas.guiart@gmail.com>
2023-10-02 09:09:52 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [react()],
+	vite: {
+		resolve: {
+			alias: {
+				'@components': '/src/components',
+				'@layouts': '/src/layouts',
+				'@styles': '/src/styles'
+			}
+		}
+	}
+});
